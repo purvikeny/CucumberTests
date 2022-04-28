556b5a3edc5b88f1e5c4bc6c46dd9253c103e2d8
Purvi Keny <purvikeny@gmail.com>
2022-04-28 18:38:50 +0530

    first git push

---
+++ TestSapient/src/test/java/Class1.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.cucumber.java.en.*;
+
+public class Class1 {
+	
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+		System.setProperty("webdriver.gecko.driver", "C://geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
+		driver.get("https://www.google.com/search?q=google");
+	    
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+	    
+	}
+
+	@When("I complete action")
+	public void i_complete_action() {
+	    
+	}
+
+	@When("some other action")
+	public void some_other_action() {
+	    
+	}
+
+	@When("yet another action")
+	public void yet_another_action() {
+	    
+	}
+
+	@Then("I validate the outcomes")
+	public void i_validate_the_outcomes() {
+	    
+	}
+
+	@Then("check more outcomes")
+	public void check_more_outcomes() {
+	    
+	}
+
+}
